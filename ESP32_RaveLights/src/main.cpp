4eaffd8d74df02ae05807e27eef67a22ac0afa8f
marioP <mario.pardo003@gmail.com>
2025-08-04 18:36:08 -0400

    basic task structure

--- ESP32_RaveLights/src/main.cpp
+++ ESP32_RaveLights/src/main.cpp
@@ -2,36 +2,44 @@ extern "C" {
     #include <stdio.h>
     #include "freertos/FreeRTOS.h"
     #include "freertos/task.h"
+    #include "freertos/queue.h"
 
     #include "driver/gpio.h"
     #include "led_strip.h"
+
     #include "esp_log.h"
     #include "esp_err.h"
 }
 
+// GPIO Definitions
 #define BUTTON1_GPIO GPIO_NUM_23
 #define BUTTON2_GPIO GPIO_NUM_22
 
-#define TEST_GPIO GPIO_NUM_21 // This is the GPIO for the LED strip, change as needed
+#define LED_STRIP_GPIO GPIO_NUM_18    
 
 //LED Strip
 #define LED_STRIP_USE_DMA  0
 #define LED_STRIP_RMT_RES_HZ 10000000
 #define LED_STRIP_MEMORY_BLOCK_WORDS 64 
-#define LED_STRIP_GPIO GPIO_NUM_18            
 #define LED_STRIP_LENGTH 50           
+
+typedef enum {
+    CMD_FUSE_WAVE,
+    CMD_BLINK_LEDS
+}LightingCommand;
+
+QueueHandle_t lightingQueue;
+
+led_strip_handle_t led_strip = NULL;
 
 
 static const char *TAG = "ESP32";
 
-
 /////// LED FUNCTIONS //////
 
-
-
-led_strip_handle_t configure_led(void)
+led_strip_handle_t configure_ledstrip(void)
 {
-    // LED strip general initialization, according to your led board design
+    
     led_strip_config_t strip_config = {
         .strip_gpio_num = LED_STRIP_GPIO, 
         .max_leds = LED_STRIP_LENGTH,     
@@ -44,11 +52,11 @@ led_strip_handle_t configure_led(void)
 
     // LED strip backend configuration: RMT
     led_strip_rmt_config_t rmt_config = {
-        .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
-        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
-        .mem_block_symbols = LED_STRIP_MEMORY_BLOCK_WORDS, // the memory block size used by the RMT channel
+        .clk_src = RMT_CLK_SRC_DEFAULT,    
+        .resolution_hz = LED_STRIP_RMT_RES_HZ,
+        .mem_block_symbols = LED_STRIP_MEMORY_BLOCK_WORDS, 
         .flags = {
-            .with_dma = LED_STRIP_USE_DMA,     // Using DMA can improve performance when driving more LEDs
+            .with_dma = LED_STRIP_USE_DMA,   
         }
     };
 
@@ -59,15 +67,13 @@ led_strip_handle_t configure_led(void)
     return led_strip;
 }
 
-
 void blink_leds(led_strip_handle_t led_strip, int delay_ms, int times) {
+
     for (int i = 0; i < times; i++) {
-
-
         // Turn all LEDs on (set to white color)
-          for (int j = 0; j < LED_STRIP_LENGTH; j++) {
-                ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, j, 200, 200, 200));
-            }
+        for (int j = 0; j < LED_STRIP_LENGTH; j++) {
+            ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, j, 200, 200, 200));
+        }
         ESP_ERROR_CHECK(led_strip_refresh(led_strip));
         vTaskDelay(pdMS_TO_TICKS(delay_ms));
 
@@ -102,16 +108,58 @@ void fuse_wave(led_strip_handle_t led_strip, uint8_t red, uint8_t green, uint8_t
     }
 }
 
-//  END LED FUNCTIONS ///////
+
+// Tasks
+
+void input_task(void *pvParameters)
+{
+    while(1)
+    {
+         // Check if BUTTON1 is pressed
+        if (gpio_get_level(BUTTON1_GPIO) == 1) {
+            ESP_LOGI(TAG, "Button 1 pressed");
+            LightingCommand cmd = CMD_FUSE_WAVE;
+            xQueueSend(lightingQueue, &cmd, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(200)); 
+          
+        }
+
+        // Check if BUTTON2 is pressed
+        if (gpio_get_level(BUTTON2_GPIO) == 1) {
+            ESP_LOGI(TAG, "Button 2 pressed");
+            LightingCommand cmd = CMD_BLINK_LEDS;
+            xQueueSend(lightingQueue, &cmd, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(100));  // Check every 100 ms
+
+    }
+}
 
 
+void lighting_task(void *pvParameters) {
+    LightingCommand cmd;
+    while (1) {
+        if (xQueueReceive(lightingQueue, &cmd, portMAX_DELAY)) {
+            switch (cmd) {
+                case CMD_FUSE_WAVE:
+                    fuse_wave(led_strip, 255,255,255);
+                    break;
+                case CMD_BLINK_LEDS:
+                    blink_leds(led_strip, 500, 3);
+                    break;
+            }
+        }
+    }
+}
 
+
+/// Main Code ///
 
 void setup()
 {
-    //Configure GPIO
-
-     // Configure LED GPIO as output
+    // Configure LED GPIO as output
     gpio_config_t led_io_conf = {
         .pin_bit_mask = (1ULL << LED_STRIP_GPIO),
         .mode = GPIO_MODE_OUTPUT,
@@ -130,6 +178,10 @@ void setup()
         .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&button_io_conf);
+
+
+
+
 }
 
 
@@ -138,7 +190,7 @@ extern "C" void app_main(void)
    
    setup();
 
-    led_strip_handle_t led_strip = configure_led();
+    led_strip = configure_ledstrip();
     if (led_strip == NULL) {
         ESP_LOGE(TAG, "Failed to configure LED strip");
         return;
@@ -146,24 +198,11 @@ extern "C" void app_main(void)
 
     vTaskDelay(pdMS_TO_TICKS(1000));
     fflush(stdout);
-
     ESP_LOGI(TAG, "Reached main loop");
-    while (1) {
-
-        // Check if BUTTON1 is pressed
-        if (gpio_get_level(BUTTON1_GPIO) == 1) {
-            ESP_LOGI(TAG, "Button 1 pressed, Blinking on LEDs");
-            blink_leds(led_strip, 500, 5); // Blink LEDs 5 times with 500 ms delay
-        }
-
-        // Check if BUTTON2 is pressed
-        if (gpio_get_level(BUTTON2_GPIO) == 1) {
-            ESP_LOGI(TAG, "Button 2 pressed, turning off LEDs");
-            fuse_wave(led_strip, 255, 255, 255); // Turn off all LEDs
-        }
 
 
+    lightingQueue = xQueueCreate(4, sizeof(LightingCommand));
 
-        vTaskDelay(pdMS_TO_TICKS(200));  // Check every 200 ms
-    }
+    xTaskCreate(input_task, "InputTask", 2048, NULL, 5, NULL);
+    xTaskCreate(lighting_task, "LightingTask", 2048, NULL, 5, NULL);
 }
